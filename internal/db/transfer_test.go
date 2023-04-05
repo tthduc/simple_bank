9eaaf5354765efd76d106462f10e4c26d35ecdaa
thanhduc <thanhduc736@gmail.com>
2023-04-05 22:42:15 +0700

    first commit

---
+++ internal/db/transfer_test.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createTransferT(t *testing.T) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: 1,
+		ToAccountID:   2,
+		Amount:        10,
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createTransferT(t)
+}
