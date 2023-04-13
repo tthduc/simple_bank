f6682f73781430839c4bb37e780909ae60046772
thanhduc <thanhduc736@gmail.com>
2023-04-13 22:31:12 +0700

    Update

--- internal/db/transfer_test.go
+++ internal/db/transfer_test.go
@@ -1,32 +1,33 @@
 package db
 
-import (
-	"context"
-	"github.com/stretchr/testify/require"
-	"testing"
-)
-
-func createTransferT(t *testing.T) Transfer {
-	arg := CreateTransferParams{
-		FromAccountID: 2,
-		ToAccountID:   3,
-		Amount:        10,
-	}
-
-	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, transfer)
-
-	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
-	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
-	require.Equal(t, arg.Amount, transfer.Amount)
-
-	require.NotZero(t, transfer.ID)
-	require.NotZero(t, transfer.CreatedAt)
-
-	return transfer
-}
-
-func TestCreateTransfer(t *testing.T) {
-	createTransferT(t)
-}
+//
+//import (
+//	"context"
+//	"github.com/stretchr/testify/require"
+//	"testing"
+//)
+//
+//func createTransferT(t *testing.T) Transfer {
+//	arg := CreateTransferParams{
+//		FromAccountID: 2,
+//		ToAccountID:   3,
+//		Amount:        10,
+//	}
+//
+//	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+//	require.NoError(t, err)
+//	require.NotEmpty(t, transfer)
+//
+//	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+//	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+//	require.Equal(t, arg.Amount, transfer.Amount)
+//
+//	require.NotZero(t, transfer.ID)
+//	require.NotZero(t, transfer.CreatedAt)
+//
+//	return transfer
+//}
+//
+//func TestCreateTransfer(t *testing.T) {
+//	createTransferT(t)
+//}
